e4991bb50d5f858ed4f75d8be6f7436184984204
Maksym-Kutsil <kucilmaksim@gmail.com>
2024-01-06 00:36:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -30,33 +30,33 @@ class Aquarium:
 
 
 def main():
-    aquarium1 = Aquarium(4)
-    aquarium2 = Aquarium(30)
+    non_agressive_aquarium = Aquarium(4)
+    agressive_aquarium = Aquarium(30)
 
-    fish1 = Fish("Nemo", 2, "Clownfish", 0.1, "Plankton", False, 0.5)
-    fish2 = Fish("Sharky", 5, "Great White Shark", 4.0, "Other Fish", False, 3.0)
-    fish3 = Fish("Dory", 3, "Blue Tang", 0.15, "Algae", False, 0.6)
-    fish4 = Fish("Goldie", 1, "Goldfish", 0.05, "Fish Flakes", True, 0.2)
-    fish5 = Fish("Angry", 4, "Piranha", 0.3, "Small Fish", True, 1.0)
-    fish6 = Fish("Speedy", 2, "Guppy", 0.02, "Fish Flakes", True, 0.1)
-    fish7 = Fish("Fluffy", 3, "Angelfish", 0.3, "Worms", False, 0.5)
+    fish1 = Fish("NemoFish", 2, "Clownfish", 0.1, "Plankton", False, 0.5)
+    fish2 = Fish("SharkyFish", 5, "Great White Shark", 4.0, "Other Fish", False, 3.0)
+    fish3 = Fish("DoryFish", 3, "Blue Tang", 0.15, "Algae", False, 0.6)
+    fish4 = Fish("GoldieFish", 1, "Goldfish", 0.05, "Fish Flakes", True, 0.2)
+    fish5 = Fish("AngryFish", 4, "Piranha", 0.3, "Small Fish", True, 1.0)
+    fish6 = Fish("SpeedyFish", 2, "Guppy", 0.02, "Fish Flakes", True, 0.1)
+    fish7 = Fish("FluffyFish", 3, "Angelfish", 0.3, "Worms", False, 0.5)
 
     fish_list = (fish1, fish2, fish3, fish4, fish5, fish6, fish7)
     for fish in fish_list:
         if fish.is_aggressive:
-            aquarium2.add_fish(fish)
+            agressive_aquarium.add_fish(fish)
         else:
-            aquarium1.add_fish(fish)
+            non_agressive_aquarium.add_fish(fish)
 
-    aquarium1.get_top_three_largest_fish()
-    aquarium2.get_top_three_largest_fish()
+    non_agressive_aquarium.get_top_three_largest_fish()
+    agressive_aquarium.get_top_three_largest_fish()
 
-    print("Fish in Aquarium 1:")
-    for fish in aquarium1.non_aggressive_fish[:3]:
+    print("Fish in non_agressive_aquarium:")
+    for fish in non_agressive_aquarium.non_aggressive_fish[:3]:
         print(f"{fish.name}: {fish.size} cubic meters")
 
-    print("\nFish in Aquarium 2:")
-    for fish in aquarium2.aggressive_fish[:3]:
+    print("\nFish in agressive_aquarium:")
+    for fish in agressive_aquarium.aggressive_fish[:3]:
         print(f"{fish.name}: {fish.size} cubic meters")
 
 
